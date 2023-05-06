75d7d6d70b5b1a07eaf38f5426a97c6d85e4bb0d
Layto <122397026+laytodev@users.noreply.github.com>
2023-05-06 08:51:56 -0400

    Added additional access flag methods to asm

--- asm/src/main/kotlin/io/runebox/asm/tree/FieldNode.kt
+++ asm/src/main/kotlin/io/runebox/asm/tree/FieldNode.kt
@@ -1,6 +1,8 @@
 package io.runebox.asm.tree
 
 import io.runebox.asm.util.field
+import org.objectweb.asm.Opcodes.*
+import org.objectweb.asm.Type
 import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.FieldNode
 
@@ -12,3 +14,9 @@ var FieldNode.owner: ClassNode by field()
 val FieldNode.group get() = owner.group
 
 val FieldNode.id get() = "${owner.id}.$name"
+val FieldNode.type get() = Type.getType(desc)
+
+fun FieldNode.isPublic() = (access and ACC_PUBLIC) != 0
+fun FieldNode.isStatic() = (access and ACC_STATIC) != 0
+fun FieldNode.isAbstract() = (access and ACC_ABSTRACT) != 0
+fun FieldNode.isInterface() = (access and ACC_INTERFACE) != 0
